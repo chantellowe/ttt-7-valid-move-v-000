cd372b8595d27d7da163a8e6ab394b6b6025ee72
Chantel Lowe <chantellowe17@gmail.com>
2018-11-19 07:32:24 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -10,7 +10,7 @@ end
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
     return false
-  elsif board[index] == "X" || board[index] == "O"
-    return true
+  #elsif board[index] == "X" || board[index] == "O"
+    #return true
   end
 end
