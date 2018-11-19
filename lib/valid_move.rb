ad6769ed255b95a5e4e95d76438b2bfd222504a8
Chantel Lowe <chantellowe17@gmail.com>
2018-11-19 07:35:40 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,8 +1,8 @@
 #board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def valid_move?(board, index)
-  if !position_taken?(board, index)
-    true
+  if position_taken?(board, index)
+    false
   end
 
 
